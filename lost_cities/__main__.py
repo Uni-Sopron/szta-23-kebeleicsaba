0ecd0903b652524b8d866c98f430a815a3ff1bb1
Kebelei Csaba <kebeleicsaba@gmail.com>
2023-07-07 19:12:42 +0200

    fix push

--- lost_cities/__main__.py
+++ lost_cities/__main__.py
@@ -2,6 +2,7 @@ from .controller.game_controller import GameController
 from .view.console_view import ConsoleView
 
 if __name__ == "__main__":
-    v = ConsoleView()
-    c = GameController(v)
+    pass
+    # v = ConsoleView()
+    # c = GameController(v)
     # c.main()
